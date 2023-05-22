3f2f65a0787d03eb771b6d147ad1444799c0b74b
diegoprodev <devdiegopro@gmail.com>
2023-05-22 16:14:48 -0300

    Update login.py
    adiconado icone
    aumentado tamanho da janela
    import customtkinter as ctk

--- login.py
+++ login.py
@@ -1,19 +1,18 @@
-import customtkinter
+import customtkinter as ctk
 from tkinter import *
+ctk.set_appearance_mode("dark")
+ctk.set_appearance_mode("dark blue")
 
-customtkinter.set_appearance_mode("dark")
-customtkinter.set_appearance_mode("dark blue")
-
-janela = customtkinter.CTk()
-janela.geometry("700x400")
+janela = ctk.CTk()
+janela.geometry("800x400")
 janela.title("Sistema de Login")
-janela.iconbitmap("icon.ico")
+janela.iconbitmap("icon.png")
 janela.resizable(False, False)
 
 # inserindo a imagem na tela de login
 
 img = PhotoImage(file="log.png")
-Label_img = customtkinter.CTkLabel(master=janela, image=img)
+Label_img = ctk.CTkLabel(master=janela, image=img)
 Label_img.place(x=1, y=5)
 
 
